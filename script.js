eaeae68e18f05a2804ad5c48c5adbcb2be5f7766
BlackGostt6 <gabrielsam.276@gmail.com>
2026-01-31 00:48:40 -0300

    Upgrade

--- script.js
+++ script.js
@@ -28,4 +28,64 @@ const light = document.querySelector(".cursor-light");
 document.addEventListener("mousemove", (e) => {
     light.style.left = e.clientX + "px";
     light.style.top = e.clientY + "px";
-})
+});
+
+const carousel = document.querySelector('.carousel');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let scrollAmount = 0;
+const cardWidth = 550;
+
+next.addEventListener('click', () => {
+  scrollAmount += cardWidth;
+  if(scrollAmount > (carousel.scrollWidth - carousel.clientWidth)) {
+    scrollAmount = carousel.scrollWidth - carousel.clientWidth;
+  }
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+prev.addEventListener('click', () => {
+  scrollAmount -= cardWidth;
+  if(scrollAmount < 0) scrollAmount = 0;
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+let touchStartX = 0;
+let touchEndX = 0;
+let isDragging = false;
+let dragOffset = 0;
+
+carousel.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].clientX;
+  isDragging = true;
+  carousel.style.transition = 'none';
+}, false);
+
+carousel.addEventListener('touchmove', (e) => {
+  if (!isDragging) return;
+  
+  touchEndX = e.changedTouches[0].clientX;
+  dragOffset = touchEndX - touchStartX;
+  carousel.style.transform = `translateX(-${scrollAmount - dragOffset}px)`;
+}, false);
+
+carousel.addEventListener('touchend', (e) => {
+  isDragging = false;
+  carousel.style.transition = 'transform 0.4s ease';
+  
+  const minSwipeDistance = 50;
+  
+  if (dragOffset > minSwipeDistance) {
+    scrollAmount -= cardWidth;
+    if(scrollAmount < 0) scrollAmount = 0;
+  } else if (dragOffset < -minSwipeDistance) {
+    scrollAmount += cardWidth;
+    if(scrollAmount > (carousel.scrollWidth - carousel.clientWidth)) {
+      scrollAmount = carousel.scrollWidth - carousel.clientWidth;
+    }
+  }
+  
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+  dragOffset = 0;
+}, false);
